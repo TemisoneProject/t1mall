67c8d51743dce5809fdaf609f4b9b7534b875dda
torahoo <legokbs@gmail.com>
2023-11-24 10:23:04 +0900

    first commit

---
+++ src/main/java/com/tonemall/T1mallApplication.java
@@ -0,0 +1,13 @@
+package com.tonemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1mallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1mallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonemall/T1mallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1mallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
